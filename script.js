4b75f1c7c2da5978f897fcf0fec62f6f3594b85a
AhmedMagdy790 <86210578+AhmedMagdy790@users.noreply.github.com>
2022-08-05 04:10:11 +0200

    Update script.js

--- script.js
+++ script.js
@@ -16,6 +16,16 @@ let updata = document.getElementById("updata");
 
 
 
+console.log("hello") 
+alert("hello") 
+let xReq = XMLHttpRequest(); 
+    
+    xReq. open('get','data.json'); 
+    xReq.onload = function (){
+        let xData = JSON.parse(xReq.responseText); 
+        alert(xData[0]) 
+        } 
+              xReq.send() 
 
 
 
